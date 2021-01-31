19918945aa2b3c304a7f8966dfa469b02da222b0
Javad Mnjd <javad.mnjd@hotmail.com>
2021-01-31 20:36:44 +0330

    create FetchWrapper

---
+++ FetchWrapper.ts
@@ -0,0 +1,45 @@
+import { CookieJar } from "./CookieJar.ts";
+
+export type fetchWrapOptions = {
+  cookieJar?: CookieJar;
+};
+
+export function wrapFetch(options?: fetchWrapOptions) {
+  let cookieJar: CookieJar;
+  if (options?.cookieJar) {
+    cookieJar = options.cookieJar;
+  } else {
+    cookieJar = new CookieJar();
+  }
+
+  async function wrappedFetch(
+    input: string | Request | URL,
+    init?: RequestInit | undefined,
+  ) {
+    // let fetch handle the error
+    if (!input) {
+      return await fetch(input);
+    }
+    const cookieString = cookieJar.getCookieString(input);
+    const interceptedInit = init || {};
+    if (interceptedInit) {
+      interceptedInit.headers = Object.assign(
+        interceptedInit.headers || {},
+        cookieString
+          ? {
+            cookie: cookieString,
+          }
+          : {},
+      );
+    }
+    const response = await fetch(input, interceptedInit);
+    response.headers.forEach((value, key) => {
+      if (key.toLowerCase() === "set-cookie") {
+        cookieJar.setCookie(value, input);
+      }
+    });
+    return response;
+  }
+
+  return wrappedFetch;
+}
